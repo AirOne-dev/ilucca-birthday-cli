e1d969bd01a00df07e98e5cb1fcfa418d92610a1
AirOne-dev <erwan1207@gmail.com>
2023-07-13 16:12:33 +0200

    reformat code

--- ilucca_birthday_cli/__init__.py
+++ ilucca_birthday_cli/__init__.py
@@ -4,24 +4,53 @@ import schedule
 import time
 from . import birthday_utils
 
-locale.setlocale(locale.LC_TIME, 'fr_FR.UTF-8')
+locale.setlocale(locale.LC_TIME, "fr_FR.UTF-8")
 
 
 # Pour exécuter le script en ligne de commande et choisir
 # si on veut afficher le prochain anniversaire ou tous les anniversaires
 def cli():
-    parser = argparse.ArgumentParser(description='Affiche les prochains anniversaires.', add_help=False, usage='python3 %(prog)s [-h] [--next | --all | --today]')
-    parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS, help='Affiche ce message d\'aide.')
+    parser = argparse.ArgumentParser(
+        description="Affiche les prochains anniversaires.",
+        add_help=False,
+        usage="python3 %(prog)s [-h] [--next | --all | --today]",
+    )
+    parser.add_argument(
+        "-h",
+        "--help",
+        action="help",
+        default=argparse.SUPPRESS,
+        help="Affiche ce message d'aide.",
+    )
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('-n', '--next', action='store_true', help='Affiche le prochain anniversaire à venir.')
-    group.add_argument('-a', '--all', action='store_true', help='Affiche tous les anniversaires à venir.')
-    group.add_argument('-t', '--today', action='store_true', help='Affiche tous les anniversaires du jour.')
+    group.add_argument(
+        "-n",
+        "--next",
+        action="store_true",
+        help="Affiche le prochain anniversaire à venir.",
+    )
+    group.add_argument(
+        "-a",
+        "--all",
+        action="store_true",
+        help="Affiche tous les anniversaires à venir.",
+    )
+    group.add_argument(
+        "-t",
+        "--today",
+        action="store_true",
+        help="Affiche tous les anniversaires du jour.",
+    )
     args = parser.parse_args()
 
     if not any(vars(args).values()):
         parser.print_help()
         exit()
-    print(birthday_utils.get_birthdays(returnNext=args.next, returnAll=args.all, returnToday=args.today))
+    print(
+        birthday_utils.get_birthdays(
+            returnNext=args.next, returnAll=args.all, returnToday=args.today
+        )
+    )
 
 
 # Affiche le prochain anniversaire à venir
@@ -32,6 +61,7 @@ def next():
 # Affiche tous les anniversaires à venir
 def all():
     print(birthday_utils.get_birthdays(returnAll=True))
+
 
 # Affiche les anniversaires du jour
 def today():
@@ -45,6 +75,7 @@ def slack():
 
 def update_data():
     birthday_utils.update_data()
+
 
 # Si le script est exécuté directement, on le lance en mode cli
 if __name__ == "__main__":
--- ilucca_birthday_cli/birthday_utils.py
+++ ilucca_birthday_cli/birthday_utils.py
@@ -7,6 +7,7 @@ from slack_sdk.errors import SlackApiError
 import configparser
 import requests
 
+
 # Retourne le nombre de jours restants avant un anniversaire
 def left_days(birthday):
     today = datetime.today()
@@ -20,55 +21,109 @@ def left_days(birthday):
 
 
 # Retourne la liste des anniversaires à venir ou le prochain anniversaire
-def get_birthdays(returnAll=False, returnNext=False, returnToday=False, prettyPrint=True):
+def get_birthdays(
+    returnAll=False, returnNext=False, returnToday=False, prettyPrint=True
+):
     nbParams = returnAll + returnNext + returnToday
     if nbParams > 1:
-        print("⚠️ Erreur: get_birthdays: returnAll et returnNext et returnToday ne peuvent pas être True en même temps")
+        print(
+            "⚠️ Erreur: get_birthdays: returnAll et returnNext et returnToday ne peuvent pas être True en même temps"
+        )
         exit(1)
     if nbParams == 0:
-        print("⚠️ Erreur: get_birthdays: returnAll ou returnNext ou returnToday doit être défini")
+        print(
+            "⚠️ Erreur: get_birthdays: returnAll ou returnNext ou returnToday doit être défini"
+        )
         exit(1)
 
     try:
-        with open(os.path.join(os.path.dirname(__file__), 'ilucca_data.json')) as json_data_file:
+        with open(
+            os.path.join(os.path.dirname(__file__), "ilucca_data.json")
+        ) as json_data_file:
             data = json.load(json_data_file)
     except FileNotFoundError:
-        print("⚠️ Erreur: ilucca_data.json introuvable, veuillez exécuter la commande 'poetry run update_data'")
+        print(
+            "⚠️ Erreur: ilucca_data.json introuvable, veuillez exécuter la commande 'poetry run update_data'"
+        )
         exit(1)
 
     # Converti les données JSON en une liste d'anniversaires
-    birthdays = [{"name": item["name"], "birthday": item["birthDate"], 'firstName': item['firstName'], 'lastName': item['lastName']} for item in data["data"]["items"]]
+    birthdays = [
+        {
+            "name": item["name"],
+            "birthday": item["birthDate"],
+            "firstName": item["firstName"],
+            "lastName": item["lastName"],
+        }
+        for item in data["data"]["items"]
+    ]
 
     today = datetime.today()
 
-    returnedText = "\nLe prochain anniversaire: \n\n" if returnNext else "\nLes prochains anniversaires: \n\n" if returnAll else "\nLes anniversaires du jour: \n\n" if returnToday else ""
+    returnedText = (
+        "\nLe prochain anniversaire: \n\n"
+        if returnNext
+        else "\nLes prochains anniversaires: \n\n"
+        if returnAll
+        else "\nLes anniversaires du jour: \n\n"
+        if returnToday
+        else ""
+    )
     returnedBirthday = []
-    
+
     for day in birthdays:
-        birthday = datetime.strptime(day['birthday'], "%Y-%m-%dT%H:%M:%S")
+        birthday = datetime.strptime(day["birthday"], "%Y-%m-%dT%H:%M:%S")
         # Calcule le nombre de jours restants pour chaque anniversaire et l'ajoute à l'objet anniversaire
-        day['remaining_days'] = left_days(birthday)
+        day["remaining_days"] = left_days(birthday)
         # Calcule l'âge que la personne fêtera
-        day['age'] = relativedelta(datetime.now(), birthday).years
+        day["age"] = relativedelta(datetime.now(), birthday).years
 
         # Si l'anniversaire n'est pas aujourd'hui, ajouter 1 an à l'âge
-        day['age'] += 1 if not (today.month == birthday.month and today.day == birthday.day and today.year == birthday.year) else 0
+        day["age"] += (
+            1
+            if not (
+                today.month == birthday.month
+                and today.day == birthday.day
+                and today.year == birthday.year
+            )
+            else 0
+        )
 
     # Trie les anniversaires selon le nombre de jours restants
-    birthdays = sorted(birthdays, key=lambda k: k['remaining_days'])
+    birthdays = sorted(birthdays, key=lambda k: k["remaining_days"])
 
     for day in birthdays:
-        birthday = datetime.strptime(day['birthday'], "%Y-%m-%dT%H:%M:%S")
+        birthday = datetime.strptime(day["birthday"], "%Y-%m-%dT%H:%M:%S")
         if returnAll:
-            returnedText += "🗓️  {0:<2} {1:<15} 🧍 {2:<25} 🎂 {3:<10} ⌛ {4:<3} jours\n".format(birthday.day, birthday.strftime('%B'), day['name'], day['age'], day['remaining_days'])
+            returnedText += (
+                "🗓️  {0:<2} {1:<15} 🧍 {2:<25} 🎂 {3:<10} ⌛ {4:<3} jours\n".format(
+                    birthday.day,
+                    birthday.strftime("%B"),
+                    day["name"],
+                    day["age"],
+                    day["remaining_days"],
+                )
+            )
             returnedBirthday.append(day)
         elif returnNext:
-            returnedText += "🗓️  {0:<2} {1:<15} 🧍 {2:<25} 🎂 {3:<10} ⌛ {4:<3} jours\n".format(birthday.day, birthday.strftime('%B'), day['name'], day['age'], day['remaining_days'])
+            returnedText += (
+                "🗓️  {0:<2} {1:<15} 🧍 {2:<25} 🎂 {3:<10} ⌛ {4:<3} jours\n".format(
+                    birthday.day,
+                    birthday.strftime("%B"),
+                    day["name"],
+                    day["age"],
+                    day["remaining_days"],
+                )
+            )
             returnedBirthday.append(day)
             break
-        elif returnToday and birthday.day == today.day and birthday.month == today.month:
-            day['age'] -= 1
-            returnedText += "🎂 {0:>3} ans {1:<2} 🧍 {2:<25}\n".format(day['age'], '', day['name'])
+        elif (
+            returnToday and birthday.day == today.day and birthday.month == today.month
+        ):
+            day["age"] -= 1
+            returnedText += "🎂 {0:>3} ans {1:<2} 🧍 {2:<25}\n".format(
+                day["age"], "", day["name"]
+            )
             returnedBirthday.append(day)
 
     if prettyPrint and len(returnedBirthday) == 0:
@@ -82,15 +137,23 @@ def send_today_birthday_to_slack():
     current_birthdays = get_birthdays(returnToday=True, prettyPrint=False)
     if len(current_birthdays) > 0:
         for birthday in current_birthdays:
-            user_id = get_slack_id_from_name(birthday['firstName'], birthday['lastName'])
+            user_id = get_slack_id_from_name(
+                birthday["firstName"], birthday["lastName"]
+            )
             if user_id:
-                res = send_slack_message("🎂 Joyeux anniversaire à 🧍 <@{0}> Qui fête ses {1} ans ! 🎂".format(user_id, birthday['age']))
+                res = send_slack_message(
+                    "🎂 Joyeux anniversaire à 🧍 <@{0}> Qui fête ses {1} ans ! 🎂".format(
+                        user_id, birthday["age"]
+                    )
+                )
                 if res[0]:
                     print("✅ Message envoyé sur Slack : " + res[1])
                 else:
                     print("⚠️ Erreur lors de l'envoi sur Slack : ", res[1])
             else:
-                print(f"⚠️ Aucun utilisateur trouvé pour {birthday['firstName']} {birthday['lastName']}")
+                print(
+                    f"⚠️ Aucun utilisateur trouvé pour {birthday['firstName']} {birthday['lastName']}"
+                )
     else:
         print("Aucun anniversaire aujourd'hui.")
 
@@ -98,13 +161,13 @@ def send_today_birthday_to_slack():
 # Obtient la liste des utilisateurs du workspace Slack
 def get_slack_users():
     config = configparser.ConfigParser()
-    config.read('config.ini')
+    config.read("config.ini")
 
-    client = WebClient(token=config.get('Slack', 'slack_token'))
+    client = WebClient(token=config.get("Slack", "slack_token"))
 
     try:
         response = client.users_list()
-        return [True, response['members']]
+        return [True, response["members"]]
     except SlackApiError as e:
         return [False, e.response["error"]]
 
@@ -115,8 +178,8 @@ def get_slack_id_from_name(first_name, last_name):
     if res[0]:
         users = res[1]
         for user in users:
-            if 'real_name' in user and user['real_name'] == f"{first_name} {last_name}":
-                return user['id']
+            if "real_name" in user and user["real_name"] == f"{first_name} {last_name}":
+                return user["id"]
         return None
     else:
         print("⚠️ Erreur lors de l'obtention de la liste des utilisateurs : ", res[1])
@@ -126,12 +189,12 @@ def get_slack_id_from_name(first_name, last_name):
 # Envoi un message sur Slack
 def send_slack_message(message):
     config = configparser.ConfigParser()
-    config.read('config.ini')
+    config.read("config.ini")
 
-    client = WebClient(token=config.get('Slack', 'slack_token'))
+    client = WebClient(token=config.get("Slack", "slack_token"))
 
     try:
-        client.chat_postMessage(channel=config.get('Slack', 'channel_id'), text=message)
+        client.chat_postMessage(channel=config.get("Slack", "channel_id"), text=message)
         return [True, message]
     except SlackApiError as e:
         return [False, e.response["error"]]
@@ -139,15 +202,21 @@ def send_slack_message(message):
 
 def update_data():
     config = configparser.ConfigParser()
-    config.read('config.ini')
+    config.read("config.ini")
 
-    iluccaUrl = config.get('ilucca', 'ilucca_api_url') + '/users/birthday?fields=name,firstname,lastname,picture.href,birthDate&startsOn={0}-01-01&endsOn={1}-01-01'.format(datetime.today().year, datetime.today().year + 1)
-    cookies = {'authToken': config.get('ilucca', 'ilucca_auth_token')}
+    iluccaUrl = config.get(
+        "ilucca", "ilucca_api_url"
+    ) + "/users/birthday?fields=name,firstname,lastname,picture.href,birthDate&startsOn={0}-01-01&endsOn={1}-01-01".format(
+        datetime.today().year, datetime.today().year + 1
+    )
+    cookies = {"authToken": config.get("ilucca", "ilucca_auth_token")}
 
     response = requests.get(iluccaUrl, cookies=cookies)
     if response.status_code == 200:
-        with open(os.path.join(os.path.dirname(__file__), 'ilucca_data.json'), 'wb') as json_data_file:
+        with open(
+            os.path.join(os.path.dirname(__file__), "ilucca_data.json"), "wb"
+        ) as json_data_file:
             json_data_file.write(response.content)
-        print('ilucca_data.json updated successfully.')
+        print("ilucca_data.json updated successfully.")
     else:
-        print('⚠️ Error updating ilucca_data.json. Status code:', response.status_code)
+        print("⚠️ Error updating ilucca_data.json. Status code:", response.status_code)
